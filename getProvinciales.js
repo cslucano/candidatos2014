1564ba04f28d9db2505291d68745eaa7e15bd1fe
Carlos Salvatierra <cslucano@gmail.com>
2014-07-31 19:23:31 -0500

    added blocking crawling

--- getProvinciales.js
+++ getProvinciales.js
@@ -8,56 +8,43 @@ var selProvincia = '#ddlProvincia';
 var selDistrito = '#ddlDistrito';
 var submitFunction = 'p_Busca();';
 
-casper.start('http://200.48.102.68/sipesg_erm2014/wf_BuscarListaCandidato_HV.aspx', function () {
+casper.start('http://200.48.102.68/sipesg_erm2014/wf_BuscarListaCandidato_HV.aspx', function() {
     this.echo('scrapping lista de candidatos 2014');
     this.echo('título - ' + this.getTitle());
     var tiposEleccion = this.evaluate(utils.getTiposEleccion);
-    for (var i=0; i< tiposEleccion.length; i++) {
-      tipoEleccion = tiposEleccion[i];
-      this.echo('Tipo de Elección ' + tipoEleccion['content']);
+    
+    this.each(tiposEleccion, function(self, tipoEleccion) {
+        this.then(function() { 
+            self.echo('Tipo de Elección ' + tipoEleccion['content']);
 
-      if(tipoEleccion['content'].indexOf('PROV') >= 0) {
-        this.fill('form#wf_BuscarListaCandidato_HV', {
-          ddlTipoEleccion : tipoEleccion['value']
-        }, false);
+            if(tipoEleccion['content'].indexOf('PROV') >= 0) {
 
-        this.waitFor(
-          function check() {
-            return this.evaluate(function() {
-              return document.querySelectorAll('#ddlDepartamento option').length > 2;
-            });
-          }, 
-          function then() {
-            var dpts = this.evaluate(utils.getDepartamentos);
-            for (var j=0; j< dpts.length; j++) {
-              departamento = dpts[j];
+                this.fill('form#wf_BuscarListaCandidato_HV', {
+                  ddlTipoEleccion : tipoEleccion['value']
+                }, true);
 
-                if(departamento['value'] >= 0) {
-                  this.echo('  Departamento ' + departamento['content']);
-                  this.fill('form#wf_BuscarListaCandidato_HV', {
-                    ddlDepartamento : departamento['value']
-                  }, false);
+                var dpts = this.evaluate(utils.getDepartamentos);
+
+                this.each(dpts, function(self, departamento) {
+                    this.then(function() { 
+                        if(departamento['value'] >= 0) {
+                            this.echo('  Departamento ' + departamento['content']);
+                            this.fill('form#wf_BuscarListaCandidato_HV', {
+                              ddlDepartamento : departamento['value']
+                            }, true);
  
-                  this.waitFor(
-                    function check() {
-                      return this.evaluate(function() {
-                        return document.querySelectorAll('#ddlProvincia option').length > 2;
-                      });
-                    },
-                    function then() {
-                      var prvs = this.evaluate(utils.getProvincias);
-                      for (var k=0; k< prvs.length; k++) {
-                        this.echo('    ' + prvs[k]['value'] + ' ' + prvs[k]['content']);
-                      }
-                    }
-                  );
-                }
+                            var prvs = this.evaluate(utils.getProvincias);
+
+                            this.each(prvs, function(self, provincia) {
+                                this.echo('    ' + provincia['value'] + ' ' + provincia['content']);
+                            });
+                        }
+                    });
+                }); //end each dpts
 
             }
-          }
-        );
-      }
-    }
+        });
+    }); // End each TipoElección
 });
 
 
